aea1f19aed043e19801e31d8512f07c91413157b
TeejMcSteez <teejmcsteez@gmail.com>
2026-01-21 23:52:43 -0500

    Completed rest of project, added radio group with radio buttons added different event listeners depending on the clicked button

--- app/src/main/java/com/example/views2_4180_121/MainActivity.kt
+++ app/src/main/java/com/example/views2_4180_121/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.views2_4180_121
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,50 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val convText: TextView = findViewById(R.id.convRes)
+        val inputText: TextInputEditText = findViewById(R.id.tempInput)
+        val reset: Button = findViewById(R.id.button)
+        val calc: Button = findViewById(R.id.button2)
+        val c2f: RadioButton = findViewById(R.id.cTof)
+        val f2c: RadioButton = findViewById(R.id.fToc)
+
+        calc.setOnClickListener {
+            Toast.makeText(this, "Please enter a temperature to convert", Toast.LENGTH_LONG).show()
+        }
+
+
+        c2f.setOnCheckedChangeListener { _, _ ->
+            calc.setOnClickListener {
+                calc.setOnClickListener {
+                    try {
+                        val temp: Float = inputText.text.toString().toFloat()
+                        val conv: Float = (temp - 32) * 5/9
+                        val txt = "${conv}C"
+                        convText.text = txt
+                    } catch (e: NumberFormatException) {
+                        Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+        f2c.setOnCheckedChangeListener { _, _ ->
+            calc.setOnClickListener {
+                try {
+                    val temp: Float = inputText.text.toString().toFloat()
+                    val conv: Float = (temp * 9/5) + 32
+                    val txt = "${conv}F"
+                    convText.text = txt
+                } catch (e: NumberFormatException) {
+                    Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        reset.setOnClickListener {
+            convText.text = "N/A"
+            inputText.text?.clear()
+        }
+
     }
 }
